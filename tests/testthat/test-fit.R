518aeacc4ccfdf730ae21c0fab3a7b23f62cb079
Jorge Cimentada <cimentadaj@gmail.com>
2020-03-06 16:13:32 +0100

    Tests that order of plugging to tidyflow is unimportant for result

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -95,3 +95,34 @@ test_that("cannot fit without a fit stage", {
   expect_error(fit(tidyflow), "must have a model")
 })
 
+
+test_that("can `fit()` regardless of order", {
+  mod <- parsnip::linear_reg()
+  mod <- parsnip::set_engine(mod, "lm")
+  rcp <- ~ recipes::recipe(.x, mpg ~ cyl)
+
+  # 'Correct order'
+  tflow <- tidyflow(mtcars, seed = 23151)
+  tflow <- plug_split(tflow, rsample::initial_split)
+  tflow <- plug_recipe(tflow, rcp)
+  tflow <- plug_resample(tflow, rsample::vfold_cv)  
+  tflow <- plug_model(tflow, mod)
+  result <- fit(tflow)
+
+  expect_is(result$fit$fit$tuning, "rset")
+
+
+  # 'Random order'
+  tflow2 <- tidyflow(seed = 23151)
+  tflow2 <- plug_model(tflow2, mod)
+  tflow2 <- plug_resample(tflow2, rsample::vfold_cv)  
+  tflow2 <- plug_recipe(tflow2, rcp)
+  tflow2 <- plug_split(tflow2, rsample::initial_split)
+  tflow2 <- plug_data(tflow2, mtcars)
+  result2 <- fit(tflow2)
+
+  expect_is(result2$fit$fit$tuning, "rset")
+
+  expect_equal(rsplit2df(result),
+               rsplit2df(result2))
+})
