d8eb0960be278b27a95a6d0710d449da33a62ccf
Jorge Cimentada <cimentadaj@gmail.com>
2020-03-10 15:53:19 +0100

    Adds parameters.tidyflow

--- R/parameters.R
+++ R/parameters.R
@@ -11,25 +11,31 @@
 #' 5 + 5
 #' 
 #' @export
-## parameters.tidyflow <- function(x, ...) {
-##   model <- tidyflow::pull_tflow_spec(x)
-##   param_data <- dials::parameters(model)
-##   if (has_preprocessor_recipe(x)) {
-##     if (has_raw_data(x)) {
-##       recipe <- pull_tflow_preprocessor(x)(x$pre$mold)
-##       recipe_param_data <- dials::parameters(recipe)
-##       param_data <- dplyr::bind_rows(param_data, recipe_param_data)
-##     } else {
-##       rlang::abort("The raw data must be specified to extract the parameters of the recipe. Did you want `plug_data`?") #nolintr
-##     }
-##   }
+parameters.tidyflow <- function(x, ...) {
 
-##   dials::parameters_constr(
-##     param_data$name,
-##     param_data$id,
-##     param_data$source,
-##     param_data$component,
-##     param_data$component_id,
-##     param_data$object
-##   )
-## }
+  model <- try(tidyflow::pull_tflow_spec(x), silent = TRUE)
+  if (inherits(model, "try-error")) {
+    param_data <- dials::parameters(list())
+  } else {
+    param_data <- tune::parameters(model)
+  }
+
+  if (has_preprocessor_recipe(x)) {
+    if (has_raw_data(x)) {
+      recipe <- pull_tflow_preprocessor(x)(pull_tflow_mold(x))
+      recipe_param_data <- tune::parameters(recipe)
+      param_data <- rbind(param_data, recipe_param_data)
+    } else {
+      rlang::abort("The raw data must be specified to extract the parameters of the recipe. Did you want `plug_data`?") #nolintr
+    }
+  }
+
+  dials::parameters_constr(
+    param_data$name,
+    param_data$id,
+    param_data$source,
+    param_data$component,
+    param_data$component_id,
+    param_data$object
+  )
+}
