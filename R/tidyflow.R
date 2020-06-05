6ed07b0efbcf70c23e213251b67edc30e5c38872
Jorge Cimentada <cimentadaj@gmail.com>
2020-06-05 09:12:58 +0200

    Rewrite entire tidyflow printing

--- R/tidyflow.R
+++ R/tidyflow.R
@@ -75,14 +75,12 @@ is_tidyflow <- function(x) {
 #' @export
 print.tidyflow <- function(x, ...) {
   print_header(x)
-  print_preprocessor(x)
-  print_model(x)
-  # cat_line("")
-  # print_postprocessor(x)
   invisible(x)
 }
 
 print_header <- function(x) {
+
+  # Print header
   if (x$trained) {
     trained <- " [trained]"
   } else {
@@ -93,7 +91,24 @@ print_header <- function(x) {
   header <- cli::rule(header, line = 2)
 
   cat_line(header)
+  # End print header
 
+  print_data(x)
+  print_split(x)
+  print_formula_recipe(x)
+  print_resample(x)
+  print_grid(x)
+  print_spec(x)
+
+  if ((has_fit(x)) | has_fit_tuning(x)) {
+    cat_line(cli::rule("Results", line = 2))
+    print_model_resample(x)
+  }
+
+  invisible(x)
+}
+
+print_data <- function(x) {
   data_msg <- cli::style_italic("Data:")
 
   if (!has_raw_data(x)) {
@@ -104,93 +119,16 @@ print_header <- function(x) {
     # I include a `is.nan` in case the data frame
     # is empty and 0 / 0 equals NaN
     data_content <- glue::glue(
-      "{nrow(dt)} rows x {ncol(dt)} columns; ",
-      "{if (is.nan(data_missing)) 0 else data_missing}% NA"
+      "{number_formatter(nrow(dt))} rows x {number_formatter(ncol(dt))} columns"
     )
 
     data_msg <- glue::glue("{data_msg} {data_content}")
   }
 
   cat_line(data_msg)
-
-  preprocessor_msg <- cli::style_italic("Preprocessor:")
-
-  if (has_preprocessor_formula(x)) {
-    preprocessor <- "Formula"
-  } else if (has_preprocessor_recipe(x)) {
-    preprocessor <- "Recipe"
-  } else {
-    preprocessor <- "None"
-  }
-
-  preprocessor_msg <- glue::glue("{preprocessor_msg} {preprocessor}")
-  cat_line(preprocessor_msg)
-
-  spec_msg <- cli::style_italic("Model:")
-
-  if (has_spec(x)) {
-    spec <- class(pull_tflow_spec(x))[[1]]
-    spec <- glue::glue("{spec}()")
-  } else {
-    spec <- "None"
-  }
-
-  spec_msg <- glue::glue("{spec_msg} {spec}")
-  cat_line(spec_msg)
-
-  invisible(x)
 }
 
-print_preprocessor <- function(x) {
-  has_preprocessor_formula <- has_preprocessor_formula(x)
-  has_preprocessor_recipe <- has_preprocessor_recipe(x)
-  has_preprocessor_split <- has_preprocessor_split(x)
-  has_preprocessor_resample <- has_preprocessor_resample(x)
-  has_preprocessor_grid <- has_preprocessor_grid(x)
-
-  preprocessing <- c(
-    has_preprocessor_formula,
-    has_preprocessor_recipe,
-    has_preprocessor_split,
-    has_preprocessor_resample,
-    has_preprocessor_grid
-  )
-
-  # If all have **no** preprocessing, don't print anything
-  if (all(!preprocessing)) {
-    return(invisible(x))
-  }
-
-  # Space between Tidyflow section and Data section
-  cat_line("")
-
-  header <- cli::rule("Preprocessor")
-  cat_line(header)
-
-  if (has_preprocessor_split) {
-    print_preprocessor_split(x)
-  }
-
-  if (has_preprocessor_resample) {
-    print_preprocessor_resample(x)
-  }
-
-  if (has_preprocessor_formula) {
-    print_preprocessor_formula(x)
-  }
-
-  ## if (has_preprocessor_recipe) {
-  ##   print_preprocessor_recipe(x)
-  ## }
-
-  if (has_preprocessor_grid) {
-    print_preprocessor_grid(x)
-  }
-
-  invisible(x)
-}
-
-print_preprocessor_split <- function(x) {
+print_split <- function(x) {
   split_msg <- cli::style_italic("Split:")
 
   if (!has_preprocessor_split(x)) {
@@ -211,7 +149,31 @@ print_preprocessor_split <- function(x) {
   cat_line(split_msg)
 }
 
-print_preprocessor_resample <- function(x) {
+print_formula_recipe <- function(x) {
+  if (!(has_preprocessor_formula(x) || has_preprocessor_recipe(x))) {
+    preprocessor_msg <- cli::style_italic("Recipe/Formula: None")
+    cat_line(preprocessor_msg)
+  } else {
+    if (has_preprocessor_formula(x)) {
+      print_formula(x)
+    } else if (has_preprocessor_recipe(x)) {
+      print_recipe(x)
+    }
+  }
+}
+
+print_formula <- function(x) {
+  formula_msg <- cli::style_italic("Formula: ")
+  formula <- pull_tflow_preprocessor(x)
+  formula <- rlang::expr_text(formula)
+  cat_line(glue::glue(formula_msg, formula))
+}
+
+print_recipe <- function(x) {
+  cat_line("Recipe: available")
+}
+
+print_resample <- function(x) {
   resample_msg <- cli::style_italic("Resample:")
 
   if (!has_preprocessor_resample(x)) {
@@ -232,7 +194,7 @@ print_preprocessor_resample <- function(x) {
   cat_line(resample_msg)
 }
 
-print_preprocessor_grid <- function(x) {
+print_grid <- function(x) {
   grid_msg <- cli::style_italic("Grid:")
 
   if (!has_preprocessor_grid(x)) {
@@ -263,104 +225,30 @@ print_preprocessor_grid <- function(x) {
   }
 
   cat_line(grid_msg)
-}
+} 
 
-print_preprocessor_formula <- function(x) {
-  formula_msg <- cli::style_italic("Formula: ")
-  formula <- pull_tflow_preprocessor(x)
-  formula <- rlang::expr_text(formula)
+print_spec <- function(x) {
+  if (has_spec(x)) {
+    spec <- pull_tflow_spec(x)
 
-  cat_line(glue::glue(formula_msg, formula))
+    cat_line(cli::style_italic("Model:"))
+    print(spec)
+  } else {
+    cat_line(cli::style_italic("Model: None"))
+  }
 
   invisible(x)
 }
 
-print_preprocessor_recipe <- function(x) {
-  recipe <- pull_tflow_preprocessor(x)
-  steps <- recipe$steps
-  n_steps <- length(steps)
-
-  if (n_steps == 1L) {
-    step <- "Step"
-  } else {
-    step <- "Steps"
-  }
-
-  n_steps_msg <- glue::glue("{n_steps} Recipe {step}")
-  cat_line(n_steps_msg)
-
-  if (n_steps == 0L) {
-    return(invisible(x))
-  }
-
-  cat_line("")
-
-  step_names <- vapply(steps, pull_step_name, character(1))
-
-  if (n_steps <= 10L) {
-    cli::cat_bullet(step_names)
-    return(invisible(x))
-  }
-
-  extra_steps <- n_steps - 10L
-  step_names <- step_names[1:10]
-
-  if (extra_steps == 1L) {
-    step <- "step"
-  } else {
-    step <- "steps"
-  }
-
-  extra_dots <- "..."
-  extra_msg <- glue::glue("and {extra_steps} more {step}.")
-
-  step_names <- c(step_names, extra_dots, extra_msg)
-
-  cli::cat_bullet(step_names)
-  invisible(x)
-}
-
-pull_step_name <- function(x) {
-  step <- class(x)[[1]]
-  glue::glue("{step}()")
-}
-
-print_model <- function(x) {
-  has_spec <- has_spec(x)
-
-  if (!has_spec) {
-    return(invisible(x))
-  }
-
+print_model_resample <- function(x) {
   has_fit <- has_fit(x)
   has_tuning <- has_fit_tuning(x)
 
   # Space between Tidyflow/Preprocessor section and Model section
   cat_line("")
-
-  header <- cli::rule("Model")
-  cat_line(header)
-
-  print_spec(x)
-
-  if (has_tuning) {
-    print_tuning(x)
-    invisible(x)
-  }
-
-  if (has_fit) {
-    print_fit(x)
-    return(invisible(x))
-  }
-
-  invisible(x)
-}
-
-print_spec <- function(x) {
-  spec <- pull_tflow_spec(x)
-
-  print(spec)
-
+  if (has_tuning) print_tuning(x)
+  cat_line("")
+  if (has_fit) print_fit(x)
   invisible(x)
 }
 
@@ -371,16 +259,14 @@ print_fit <- function(x) {
   output <- utils::capture.output(fit)
   n_output <- length(output)
 
-  if (n_output < 50L) {
-    print(fit)
-    return(invisible(x))
-  }
+  if (n_output < 6L) print(fit)
 
-  n_extra_output <- n_output - 50L
-  output <- output[1:50]
+  n_extra_output <- n_output - 5L
+  output <- output[1:5]
 
   extra_output_msg <- glue::glue("and {n_extra_output} more lines.")
 
+  cat_line("Fitted model:")
   cat_line(output)
   cat_line("")
   cat_line("...")
@@ -416,4 +302,11 @@ print_tuning <- function(x) {
 
 cat_line <- function(...) {
   cat(paste0(..., collapse = "\n"), "\n", sep = "")
+}
+
+number_formatter <- function(tx) { 
+  div <- findInterval(as.numeric(gsub("\\,", "", tx)),
+                      c(0, 1e3, 1e6, 1e9, 1e12) )
+  intm <- round( as.numeric(gsub("\\,","",tx))/10^(3*(div-1)), 2)
+  paste0(intm, c("","K","M","B","T")[div] )
 }
