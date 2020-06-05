117804c91fa786e688262b2ec3bcd5fbf4abd39b
Jorge Cimentada <cimentadaj@gmail.com>
2020-06-05 11:22:57 +0200

    Revamps pull_* docs

--- R/pull.R
+++ R/pull.R
@@ -2,48 +2,49 @@
 #'
 #' @description
 #' These functions extract various elements from a tidyflow object. If they do
-#' not exist yet, an error is thrown.
+#' not exist yet, an error is thrown. **Most of these steps can only be executed
+#' after the \code{tidyflow} has been fitted**.
 #'
-#' - `pull_tflow_rawdata()` returns the complete raw/untrained data.
+#' - \code{pull_tflow_rawdata()} returns the complete raw/untrained data.
 #'
-#' - `pull_tflow_split()` returns the split object from the function specified
+#' - \code{pull_tflow_split()} returns the split object from the function specified
 #'    in \code{\link{plug_split}}.
 #'
-#' - `pull_tflow_training()` returns the training data from the split. Only
+#' - \code{pull_tflow_training()} turns the training data from the split. Only
 #'    works when a split has been specified with \code{\link{plug_split}}.
 #'    If \code{prep = TRUE}, the preprocessing (either recipe or formula)
 #'    is applied to the data.
 #'
-#' - `pull_tflow_testing()` returns the testing data from the split. Only
+#' - \code{pull_tflow_testing()} returns the testing data from the split. Only
 #'    works when a split has been specified with \code{\link{plug_split}}
 #'    If \code{prep = TRUE}, the preprocessing (either recipe or formula)
 #'    is applied to the data.
 #' 
-#' - `pull_tflow_preprocessor()` returns either the formula or recipe used
+#' - \code{pull_tflow_preprocessor()} returns either the formula or recipe used
 #'   for preprocessing. Note that in the case of the recipe it returns the
 #'   function that should be applied to the data to apply the recipe.
 #'   \code{\link{pull_tflow_prepped_recipe}} returns the recipe already applied
 #'   to the training data.
 #'
-#' - `pull_tflow_resample()` returns the resample object from the function
-#'    specified in \code{\link{plug_resample}}. This resample object already
-#'    has the preprocessor applied (either formula or recipe).
+#' - \code{pull_tflow_resample()} returns the resample object from the function
+#'    specified in \code{\link{plug_resample}}. The resample object does not
+#'    have the preprocessor applied (either formula or recipe).
 #'
-#' - `pull_tflow_grid()` returns the grid data frame from which the tuning
+#' - \code{pull_tflow_grid()} returns the grid data frame from which the tuning
 #'    parameter was made.
 #' 
-#' - `pull_tflow_prepped_recipe()` returns the prepped recipe. It is
-#'   extracted from the mold object returned from `pull_tflow_mold()`.
+#' - \code{pull_tflow_prepped_recipe()} returns the prepped recipe. It is
+#'   extracted from the mold object returned from \code{pull_tflow_mold()}.
 #'
-#' - `pull_tflow_mold()` returns the preprocessed "mold" object returned
+#' - \code{pull_tflow_mold()} returns the preprocessed "mold" object returned
 #'   from [hardhat::mold()]. It contains information about the preprocessing,
 #'   including either the prepped recipe or the formula terms object.
 #'
-#' - `pull_tflow_spec()` returns the parsnip model specification.
+#' - \code{pull_tflow_spec()} returns the parsnip model specification.
 #'
-#' - `pull_tflow_fit()` returns the parsnip model fit.
+#' - \code{pull_tflow_fit()} returns the parsnip model fit.
 #'
-#' - `pull_tflow_fit_tuning()` returns the resample result from model tuning.
+#' - \code{pull_tflow_fit_tuning()} returns the resample result from model tuning.
 #'
 #' @param x A tidyflow
 #'
@@ -61,18 +62,18 @@
 #' library(recipes)
 #' library(rsample)
 #'
-#' model <- linear_reg()
-#' model <- set_engine(model, "lm")
+#' model <- set_engine(linear_reg(), "lm")
 #'
 #' recipe <- ~ recipe(.x, mpg ~ cyl + disp) %>% step_log(disp)
 #'
-#' base_tflow <- tidyflow()
-#' base_tflow <- plug_data(base_tflow, mtcars)
-#' base_tflow <- plug_split(base_tflow, initial_split)
-#' base_tflow <- plug_model(base_tflow, model)
+#' tflow <-
+#'  mtcars %>%
+#'  tidyflow() %>%
+#'  plug_split(initial_split) %>%
+#'  plug_model(model)
 #'
-#' recipe_tflow <- plug_recipe(base_tflow, recipe)
-#' formula_tflow <- plug_formula(base_tflow, mpg ~ cyl + log(disp))
+#' recipe_tflow <- plug_recipe(tflow, recipe)
+#' formula_tflow <- plug_formula(tflow, mpg ~ cyl + log(disp))
 #'
 #' fit_recipe_tflow <- fit(recipe_tflow)
 #' fit_formula_tflow <- fit(formula_tflow)
